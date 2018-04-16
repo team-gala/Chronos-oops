611153fa98f4ae0f273cdce04635f1f5b2ecd1ea
Abigail Demsas <ademsas1026@gmail.com>
2018-04-16 18:23:16 -0400

    deal with first merge conflict

---
+++ server/api/products.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const {Product} = require('../db/models');
+module.exports = router
+
+router.get('/', (req, res, next) => {
+    Product.findAll({
+        include: [{ all: true }]
+    })
+    .then(products => res.json(products))
+    .catch(next);
+})
